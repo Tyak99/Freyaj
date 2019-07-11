45053120a1bdb56a017ba78fe23a5f8b7d5d6957
Tunde Yakub <tundenasri@gmail.com>
2019-07-11 21:30:09 +0100

    commits

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function test() {
+import express from 'express';
+
+const app = express();
+const test = () => {
   console.log('this is running');
-}
+};
 
 test();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000');
+});
